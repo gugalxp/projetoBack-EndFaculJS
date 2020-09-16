756d8523f2adaa70e413481c037f3a6592705b3f
Gustavo Arruda <Gustavoleone3456@hotmail.com>
2020-09-16 18:05:55 -0300

    Backend com Update e Delete

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,3 +1,4 @@
+// Define a utilização do model usuario e a dependência http-status
 const Usuario = require('../models/usuario');
 const status = require('http-status');
  
@@ -25,4 +26,79 @@ exports.Insert = (req, res, next) => {
         })
         //catch = registra o que queremos que aconteca quando a Promise falhar
         .catch(error => next(error));
-    };
+};
+ 
+exports.SelectAll = (req, res, next) => {
+    Usuario.findAll()
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            }
+        })
+        .catch(error => next(error));
+}
+ 
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+ 
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const salario = req.body.salario;
+    const dataNascimento = req.body.dataNascimento;
+    const ativo = req.body.ativo;
+ 
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.update({
+                    nome: nome,
+                    salario: salario,
+                    dataNascimento: dataNascimento,
+                    ativo: ativo
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+ 
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const UsuarioController = require ('../controllers/usuarioController.js');
+const UsuarioController = require('../controllers/usuarioController.js');
 const router = express.Router();
  
 router.post('/usuarios', UsuarioController.Insert);
+router.get('/usuarios', UsuarioController.SelectAll);
+router.get('/usuarios/:id', UsuarioController.SelectDetail);
+router.put('/usuarios/:id', UsuarioController.Update);
+router.delete('/usuarios/:id', UsuarioController.Delete);
+
 module.exports = router;
